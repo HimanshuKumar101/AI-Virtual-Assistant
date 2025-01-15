19b705ed7c91f373cf63d9de908bf4dd5aadf5de
Himanshu Kumar <153725590+HimanshuKumar101@users.noreply.github.com>
2025-01-16 00:26:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "YoUR API keY"; // Access the API key
+const API_KEY = "YOUR_GEMINI_API_KEY"; // Access the API key
 const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 let btn = document.querySelector("#btn");
